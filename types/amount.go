e3f799fbecc21b252cb16010a19b2ff6fd305d88
transmutateio <transmutate.io@gmail.com>
2020-09-09 10:28:11 +0100

    BigInt added to Amount type, tests

--- types/amount.go
+++ types/amount.go
@@ -3,6 +3,7 @@ package types
 import (
 	"fmt"
 	"math"
+	"math/big"
 	"strconv"
 	"strings"
 )
@@ -15,99 +16,120 @@ func (e InvalidAmountError) Error() string {
 
 type Amount string
 
-func splitAmount(amt string) (uint64, uint64, error) {
-	p := strings.Split(amt, ".")
-	if len(p) > 2 {
-		return 0, 0, InvalidAmountError(amt)
-	}
-	a, err := strconv.Atoi(p[0])
-	if err != nil {
-		return 0, 0, InvalidAmountError(amt)
-	}
-	var b int
-	if len(p) > 1 {
-		if b, err = strconv.Atoi(p[1]); err != nil {
-			return 0, 0, InvalidAmountError(amt)
+func (a Amount) Clean() Amount {
+	r := []rune(a)
+	for {
+		rsz := len(r)
+		if rsz == 0 || r[0] != '0' {
+			break
 		}
+		if rsz > 1 && r[1] == '.' {
+			break
+		}
+		r = r[1:]
 	}
-	return uint64(a), uint64(b), nil
+	for {
+		rsz := len(r)
+		if rsz == 0 || r[rsz-1] != '0' {
+			break
+		}
+		r = r[:rsz-1]
+	}
+	if rsz := len(r); rsz > 0 && r[rsz-1] == '.' {
+		r = r[:rsz-1]
+	}
+	return Amount(r)
 }
 
-func cleanAmount(s string) string {
-	for strings.HasPrefix(s, "0") {
-		s = strings.TrimPrefix(s, "0")
-	}
-	if strings.HasPrefix(s, ".") {
-		s = "0" + s
-	}
-	if !strings.Contains(s, ".") {
-		return s
-	}
-	for strings.HasSuffix(s, "0") {
-		s = strings.TrimSuffix(s, "0")
-	}
-	if strings.HasSuffix(s, ".") {
-		s = strings.TrimSuffix(s, ".")
-	}
-	return s
+func NewAmountUInt64(a uint64, d int) Amount {
+	dd := uint64(math.Pow10(d))
+	aa, bb := a/dd, a%dd
+	return Amount(fmt.Sprintf("%[1]d.%0.[3]*[2]d", aa, bb, d)).Clean()
 }
 
-func NewAmount(a uint64, p uint64) Amount {
-	d := uint64(math.Pow10(int(p)))
-	aa, bb := a/d, a%d
-	return Amount(cleanAmount(fmt.Sprintf("%[1]d.%0.[3]*[2]d", aa, bb, p)))
-}
-
-func ParseAmount(amt string) (Amount, error) {
-	if _, _, err := splitAmount(amt); err != nil {
-		return "", err
+func NewAmountBigInt(a *big.Int, d int) Amount {
+	s := a.String()
+	ssz := len(s)
+	if ssz > d {
+		intPart, decPart := s[:ssz-d], s[ssz-d:]
+		return Amount(intPart + "." + decPart).Clean()
 	}
-	return Amount(cleanAmount(amt)), nil
+	return Amount("0." + strings.Repeat("0", d-ssz) + s)
 }
 
-func (a Amount) String() string               { return cleanAmount(string(a)) }
+func (a Amount) String() string { return string(a.Clean()) }
+
 func (a Amount) MarshalJSON() ([]byte, error) { return []byte(a), nil }
 
 func (a *Amount) UnmarshalJSON(b []byte) error {
-	if _, _, err := splitAmount(string(b)); err != nil {
-		return err
+	v := Amount(b)
+	if !v.Valid() {
+		return InvalidAmountError(string(b))
 	}
-	*a = Amount(b)
+	*a = v
 	return nil
 }
 
-func (a Amount) Prec() uint64 {
+func (a Amount) Decimals() int {
 	p := strings.Split(string(a), ".")
 	if len(p) < 2 {
 		return 0
 	}
-	return uint64(len(p[1]))
+	return len(p[1])
 }
 
-func (a Amount) WithPrec(n int) Amount {
+func (a Amount) WithDecimals(d int) Amount {
 	p := strings.Split(string(a), ".")
 	if len(p) == 1 {
 		p = append(p, "")
 	}
-	if p1sz := len(p[1]); p1sz > n {
-		p[1] = p[1][:n]
+	if p1sz := len(p[1]); p1sz > d {
+		p[1] = p[1][:d]
 	} else {
-		p[1] += strings.Repeat("0", n-p1sz)
+		p[1] += strings.Repeat("0", d-p1sz)
 	}
 	return Amount(p[0] + "." + p[1])
 }
 
-func (a Amount) UInt64(p int) uint64 {
-	p1, p2, err := splitAmount(string(a.WithPrec(p)))
+func (a Amount) UInt64(d int) (uint64, error) {
+	b, err := a.BigInt(d)
 	if err != nil {
-		panic(InvalidAmountError(a))
+		return 0, err
 	}
-	return p1*uint64(math.Pow10(p)) + p2
+	if !b.IsUint64() {
+		return 0, InvalidAmountError(a)
+	}
+	return b.Uint64(), nil
+}
+
+func (a Amount) BigInt(d int) (*big.Int, error) {
+	parts := strings.Split(string(a.Clean()), ".")
+	if len(parts) > 2 {
+		return nil, InvalidAmountError(a)
+	}
+	if len(parts) == 1 {
+		parts = append(parts, strings.Repeat("0", d))
+	}
+	dsz := len(parts[1])
+	if dsz != d {
+		parts[1] += strings.Repeat("0", d-dsz)
+	}
+	r, ok := big.NewInt(0).SetString(strings.Join(parts, ""), 10)
+	if !ok {
+		return nil, InvalidAmountError(a)
+	}
+	return r, nil
 }
 
 func (a Amount) Valid() bool {
-	if _, _, err := splitAmount(string(a)); err != nil {
+	parts := strings.Split(string(a), ".")
+	if len(parts) > 2 {
 		return false
+	}
+	for _, i := range parts {
+		if _, err := strconv.Atoi(i); err != nil {
+			return false
+		}
 	}
 	return true
 }
---
+++ types/amount_test.go
@@ -0,0 +1,47 @@
+package types
+
+import (
+	"encoding/json"
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAmount(t *testing.T) {
+	stringTests := []struct {
+		a   uint64
+		d   int
+		e   string
+		ed  int
+		eds Amount
+	}{
+		{100001000, 8, "1.00001", 5, "1.00001000"},
+		{100000001, 8, "1.00000001", 8, "1.00000001"},
+		{100000000, 8, "1", 0, "1.00000000"},
+	}
+	for _, i := range stringTests {
+		amt := NewAmountUInt64(i.a, i.d)
+		require.Equal(t, i.e, amt.String())
+		require.Equal(t, i.ed, amt.Decimals())
+		require.Equal(t, i.eds, amt.WithDecimals(i.d))
+		ui, err := amt.UInt64(i.d)
+		require.NoError(t, err)
+		require.Equal(t, i.a, ui)
+		bi, err := amt.BigInt(i.d)
+		require.NoError(t, err)
+		require.Zero(t, bi.Cmp(big.NewInt(int64(i.a))))
+		require.True(t, amt.Valid())
+		b, err := json.Marshal(amt)
+		require.NoError(t, err)
+		amt2 := NewAmountBigInt(big.NewInt(int64(i.a)), i.d)
+		bi, err = amt.BigInt(i.d)
+		require.NoError(t, err)
+		bi2, err := amt2.BigInt(i.d)
+		require.NoError(t, err)
+		require.Zero(t, bi.Cmp(bi2))
+		err = json.Unmarshal(b, &amt2)
+		require.NoError(t, err)
+		require.Equal(t, amt.Clean(), amt2.Clean())
+	}
+}
